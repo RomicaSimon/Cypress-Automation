401745de4cb51e7229fde22f6ea341586d1b3f0e
RomicaSimon <85682689+RomicaSimon@users.noreply.github.com>
2022-11-11 12:04:19 +0200

    Add files via upload

---
+++ 9.Frames.cy.js
@@ -0,0 +1,39 @@
+import 'cypress-iframe'
+
+describe('handle with iframes', () => {
+
+    it('Approach 1', () => {
+
+        cy.visit('https://the-internet.herokuapp.com/iframe');
+
+        const iframe = cy.get('#mce_0_ifr').its('0.contentDocument.body').should('be.visible').then(cy.wrap);
+
+        iframe.clear().type('Welcome {ctrl+a}');
+        cy.get("[aria-label='Bold']").click();
+    })
+
+
+    it('Approach 2 by using Custom command', () => {
+
+        cy.visit('https://the-internet.herokuapp.com/iframe');
+
+        cy.getIframe('#mce_0_ifr').clear().type('Welcome {ctrl+a}');
+
+        cy.get("[aria-label='Bold']").click();
+          
+    })
+
+
+    it('Approach 3 by using Cypress-iframe plugin', () => {
+
+        cy.visit('https://the-internet.herokuapp.com/iframe');
+
+        cy.frameLoaded('#mce_0_ifr');
+
+        cy.iframe('#mce_0_ifr').clear().type('Hello world {ctrl+a}');
+
+        cy.get("[aria-label='Bold']").click();
+
+    })
+
+})
