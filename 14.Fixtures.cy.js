c311948264ac47f7a9aa8cb90d4f92971c14566f
RomicaSimon <85682689+RomicaSimon@users.noreply.github.com>
2022-11-15 16:37:45 +0200

    Add files via upload
    Add files to repo.

---
+++ 14.Fixtures.cy.js
@@ -0,0 +1,53 @@
+
+describe('MyTestSuite', () => {
+
+    // Direct access - single it block
+    it('FixturesDemoTest', () => {
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+        /*  // This is hardcoded with 'Admin' & 'admin123'
+
+        cy.get("input[placeholder='Username']").type('Admin');
+        cy.get("input[placeholder='Password']").type('admin123');
+        cy.get("button[type='submit']").click();   
+        cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").should('have.text', 'Dashboard');
+        */
+
+        cy.fixture('orangehrm').then( (data) => {
+            
+            cy.get("input[placeholder='Username']").type(data.username);
+            cy.get("input[placeholder='Password']").type(data.password);
+            cy.get("button[type='submit']").click();
+            
+            cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module")
+            .should('have.text', data.expected);
+
+        })
+    })
+
+    // Access through Hook - for multiple it blocks
+    let userdata;
+    before( () =>{
+        cy.fixture('orangehrm').then( (data) => {
+            userdata = data;
+        })
+    })
+
+    it.only('FixtureDemoTest', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+        cy.get("input[placeholder='Username']").type(userdata.username);
+        cy.get("input[placeholder='Password']").type(userdata.password);
+        cy.get("button[type='submit']").click();
+
+        cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module")
+        .should('have.text',userdata.expected);
+
+    }) 
+
+
+
+    
+
+})
